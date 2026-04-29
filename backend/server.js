744c69e5e0afa4c67ad67a9eeedce1fb69b4dd55
ravish <ravishkumar9088@.com>
2026-04-29 12:47:07 +0530

    fix oath callback

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI)
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "/auth/google/callback"
+  callbackURL: "https://codereviewer-deploy.onrender.com/auth/google/callback"
 },
 async (accessToken, refreshToken, profile, done) => {
   try {
